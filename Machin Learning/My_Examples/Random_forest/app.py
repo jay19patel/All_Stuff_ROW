a7bc096acf72d5c987ae762f0f4fc29ab02ea741
jay19patel <justjayy19@gmail.com>
2023-03-29 16:00:25 +0530

    commit

---
+++ Machin Learning/My_Examples/Random_forest/app.py
@@ -0,0 +1,9 @@
+# <!-- Use  Decidion Tree -->
+#  qution puse and te pramane agad jay 
+#  gharebthi amdavad javanu rey to pella bux ke rixa ma gharethi bus stand kato rail station te nakki thay
+#  bus to pasi bus ni tikit
+#  train to train ni tiki
+#  be  mathi choose kari ne pasi agad or 
+#  aam decidion tree step by step afad jay
+
+
